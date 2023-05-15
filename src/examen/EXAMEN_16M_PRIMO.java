58e5746e23853119e65b6357673f0c3023365fe8
Andres <Andres@DESKTOP-TUR8NBA>
2023-05-15 09:12:49 +0200

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: Iffd05674ffa370940bd5b4eb85587ed2f7f9406c

---
+++ src/examen/EXAMEN_16M_PRIMO.java
@@ -0,0 +1,16 @@
+package examen;
+
+import java.util.Scanner;
+
+public class EXAMEN_16M_PRIMO {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = 0;
+
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+
+		System.out.println("Introduce un nuemro");
+		n = s.nextInt();
+	}
+
+}
